5d5f849b42738ef62db4550dbeb75d5551b424c4
andrewskyboss <andrew.cybossky@gmail.com>
2021-07-19 23:27:49 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
