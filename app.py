fab0daeccc750a36c9a69a88611c414b771204bc
andrewskyboss <andrew.cybossky@gmail.com>
2021-07-22 20:48:38 +0000

    create app file anr test run

--- app.py
+++ app.py
@@ -16,3 +16,14 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
+
+
+@app.route("/")
+def hello():
+    return "Hello world"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
