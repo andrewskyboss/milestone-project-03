b99d79e8de408d3ba780ce3d81827d0ab006ad78
andrewskyboss <andrew.cybossky@gmail.com>
2022-09-04 17:29:33 +0000

    update

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                    existing_user["password"], request.form.get("password")):
+                existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
                     return redirect(url_for(
